75ed798ddb62b73e715c6e92aa74344cbf33806d
Ryanns7 <ryanns777@gmail.com>
2022-08-26 18:02:43 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+export function Header(){
+    return(
+        <div className="w-full flex justify-between p-4 bg-gray-400 ">
+            <h1 className="bg-blue-500">Header</h1>
+        </div>
+    )
+}
