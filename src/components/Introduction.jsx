83f46ee2a4439f3bca2b8c2db53196d26be025fd
Ryanns7 <ryanns777@gmail.com>
2022-08-28 06:29:24 -0300

    portfolio introduction

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,13 @@
+
+
+export function Introduction(){
+    return(
+        <section className="h-[95vh] w-full flex justify-center items-center flex-col">
+            <div className="p-4">
+                <h2 className=" text-3xl text-blue-500 tm:text-4xl md:text-8xl">Olá, meu nome é</h2>
+                <h1 className="my-2 font-semibold text-5xl tm:text-6xl md:text-9xl">Ryan Napoleão</h1>
+                <h3 className="text-gray-500 text-2xl tm:text-3xl md:text-7xl">Front-end Developer</h3>
+            </div>
+        </section>
+    )
+}
