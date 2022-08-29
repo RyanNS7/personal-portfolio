f68374ab7f6bc4c55968b6e37f49693f36af54f8
Ryanns7 <ryanns777@gmail.com>
2022-08-29 05:34:03 -0300

    Projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,92 @@
+import simple_store from '../assets/first_project.png'
+
+export function Projects(){
+
+    const projects = [
+        {
+            image_project: '',
+            title_project: '',
+            description_product: '',
+            link_project: ''
+        }
+    ]
+
+    return(
+        <section className="w-full">
+
+            <div className='p-4'> <h3 className="text-gray-500 text-3xl text-center font-semibold">Alguns Projetos</h3> </div>
+
+         {/* Primeiro */}
+            <div className={`p-4 w-full`}>
+                <div className=' w-full h-80 relative'>
+
+                    <div className='w-full h-full'>
+                        <img src={simple_store} className='w-full h-full' />
+                    </div>
+
+                    <div className='absolute w-full bottom-0 p-4 h-[6.25rem] hover:h-[90%] transition-all duration-300 overflow-hidden bg-theme-dark/95 flex items-center flex-col'>
+                        <div className='text-2xl text-gray-500 p-4 mb-6'>
+                            <h3>Simple e-commerce</h3>    
+                        </div>
+                        
+                        <div className='p-2'>
+                            <span>
+                                Descrição do produto
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            {/* Segundo */}
+
+            <div className={`p-4 w-full`}>
+                <div className=' w-full h-80 relative'>
+
+                    <div className='w-full h-full'>
+                        <img src={simple_store} className='w-full h-full' />
+                    </div>
+
+                    <div className='absolute w-full bottom-0 p-4 h-[6.25rem] hover:h-[90%] transition-all duration-300 overflow-hidden bg-theme-dark/95 flex items-center flex-col'>
+                        <div className='text-2xl text-gray-500 p-4 mb-6'>
+                            <h3>Simple e-commerce</h3>    
+                        </div>
+                        
+                        <div className='p-2'>
+                            <span>
+                                Descrição do produto
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            {/* Terceiro */}
+
+            <div className={`p-4 w-full`}>
+                <div className=' w-full h-80 relative'>
+
+                    <div className='w-full h-full'>
+                        <img src={simple_store} className='w-full h-full' />
+                    </div>
+
+                    <div className='absolute w-full bottom-0 p-4 h-[6.25rem] hover:h-[90%] transition-all duration-300 overflow-hidden bg-theme-dark/95 flex items-center flex-col'>
+                        <div className='text-2xl text-gray-500 p-4 mb-6'>
+                            <h3>Simple e-commerce</h3>    
+                        </div>
+                        
+                        <div className='p-2'>
+                            <span>
+                                Descrição do produto
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    )
+}
