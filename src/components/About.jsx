9159ea4933d1090d35b59057557b6ee6745f6ad9
Ryanns7 <ryanns777@gmail.com>
2022-08-29 05:35:28 -0300

    About

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+
+
+export function About(){
+    return(
+        <section className="w-full bg-red-500">
+
+            <div className="w-full text-center p-4 text-3xl">
+                <h3 className="text-gray-500">Sobre Mim</h3>
+            </div>
+
+            <div className="p-4 w-full">
+
+            </div>
+            
+        </section>
+    )
+}
