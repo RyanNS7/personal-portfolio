5c319abdbaff9d67fc0fcd92742c9f7f286f840a
Ryanns7 <ryanns777@gmail.com>
2022-08-31 04:19:39 -0300

    Add cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,112 +1,42 @@
 import simple_store from '../assets/first_project.png'
+import { Card } from './Card'
+import { FaReact, FaNodeJs } from 'react-icons/fa'
+import { SiPostgresql, SiStyledcomponents } from 'react-icons/si'
 
 export function Projects(){
 
     const projects = [
         {
-            image_project: '',
-            title_project: '',
-            description_product: '',
-            link_project: ''
-        }
+            image_project: simple_store,
+            title_project: 'Simple e-commerce',
+            description: 'Um projeto fullstack de um comercio eletronico de perifericos cujo o objetivo foi atribuir os conceitos basico de um ecommerce.',
+            languages_used: [<FaReact />, <FaNodeJs />, <SiPostgresql />, <SiStyledcomponents /> ],
+            development: 'Em Desenvolvimento',
+            link_project: 'https://simple-ecommerce-pg.netlify.app/'
+        },
+        {},{},{},{},{}
     ]
 
     return(
-        <section className="w-full">
+        <section className="w-full mt-4">
 
-            <div className='p-4'> <h3 className="text-gray-500 text-3xl text-center font-semibold">Alguns Projetos</h3> </div>
+            <div className='p-4'> <h3 className="text-gray-500 text-3xl text-center font-semibold">Projetos Desenvolvidos</h3> </div>
 
-         {/* Primeiro */}
             <div className={`p-4 w-full tm:grid md:grid-cols-2 md:gap-4 lg:grid-cols-3`}>
-                {/* Primeiro */}
-                <div className=' w-full h-80 relative mb-4'>
+                {projects.map((project, key) => {
+                    if(Object.keys(project).length == 0){
 
-                    <div className='w-full h-full'>
-                        <img src={simple_store} className='w-full h-full' />
-                    </div>
-
-                    <div className='absolute w-full bottom-0 p-4 h-[6.25rem] hover:h-[90%] transition-all duration-300 overflow-hidden bg-theme-dark/95 flex items-center flex-col'>
-                        <div className='text-2xl text-gray-500 p-4 mb-6'>
-                            <h3>Simple e-commerce</h3>    
-                        </div>
-                        
-                        <div className='p-2'>
-                            <span>
-                                Descrição do produto
-                            </span>
-                        </div>
-                    </div>
-
-                </div>
-
-                {/* Segundo */}
-
-                <div className=' w-full h-80 relative mb-4'>
-
-                    <div className='w-full h-full'>
-                        <img src={simple_store} className='w-full h-full' />
-                    </div>
-
-                    <div className='absolute w-full bottom-0 p-4 h-[6.25rem] hover:h-[90%] transition-all duration-300 overflow-hidden bg-theme-dark/95 flex items-center flex-col'>
-                        <div className='text-2xl text-gray-500 p-4 mb-6'>
-                            <h3>Simple e-commerce</h3>    
-                        </div>
-                        
-                        <div className='p-2'>
-                            <span>
-                                Descrição do produto
-                            </span>
-                        </div>
-                    </div>
-
-                </div>
-
-                {/* Terceiro */}
-
-                <div className=' w-full h-80 relative'>
-
-                    <div className='w-full h-full'>
-                        <img src={simple_store} className='w-full h-full' />
-                    </div>
-
-                    <div className='absolute w-full bottom-0 p-4 h-[6.25rem] hover:h-[90%] transition-all duration-300 overflow-hidden bg-theme-dark/95 flex items-center flex-col'>
-                        <div className='text-2xl text-gray-500 p-4 mb-6'>
-                            <h3>Simple e-commerce</h3>    
-                        </div>
-                        
-                        <div className='p-2'>
-                            <span>
-                                Descrição do produto
-                            </span>
-                        </div>
-                    </div>
-
-                </div>
-
-                {/* Quarto */}
-
-                <div className=' w-full h-80 relative mb-4'>
-
-                    <div className='w-full h-full'>
-                        <img src={simple_store} className='w-full h-full' />
-                    </div>
-
-                    <div className='absolute w-full bottom-0 p-4 h-[6.25rem] hover:h-[90%] transition-all duration-300 overflow-hidden bg-theme-dark/95 flex items-center flex-col'>
-                        <div className='text-2xl text-gray-500 p-4 mb-6'>
-                            <h3>Simple e-commerce</h3>    
-                        </div>
-                        
-                        <div className='p-2'>
-                            <span>
-                                Descrição do produto
-                            </span>
-                        </div>
-                    </div>
-
-                </div>                
+                     return <div className=' w-full h-80 relative mb-4 cursor-pointer'>
+                                <div className='w-full h-full bg-black flex items-center justify-center flex-col'>
+                                    <span className='text-3xl mb-4 text-center p-2'>Projeto em Desenvolvimento</span>
+                                </div>
+                            </div>      
+                    }else{
+                        return <a href={project.link_project} target='_BLANK' ><Card project={project} key={key} /> </a>
+                    }
+                })}
+          
             </div>
-
-
 
         </section>
     )
